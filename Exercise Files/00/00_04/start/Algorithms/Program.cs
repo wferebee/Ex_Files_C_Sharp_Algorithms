b1ecbc2e05bd248f070cbcc5e53b14b1466026cb
William F <wferebee@captechconsulting.com>
2022-06-04 01:59:09 -0400

    added a stopwatch to measure rough performance

--- Exercise Files/00/00_04/start/Algorithms/Program.cs
+++ Exercise Files/00/00_04/start/Algorithms/Program.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 
-namespace Algorithms {
+namespace Algorithms
+{
 
-    class Program {
-        static int findMaximum(int a, int b, int c) {
-            if (a > b) {
-                if (a > c || a == c) {
+    class Program
+    {
+        static int findMaximum(int a, int b, int c)
+        {
+            if (a > b)
+            {
+                if (a > c || a == c)
+                {
                     return a;
                 }
             }
 
-            if (b > c) {
-                if (b > a || a == b) {
+            if (b > c)
+            {
+                if (b > a || a == b)
+                {
                     return b;
                 }
             }
@@ -19,12 +26,17 @@ namespace Algorithms {
             return c;
         }
 
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
             Console.WriteLine(findMaximum(1, 2, 3));
             Console.WriteLine(findMaximum(8, 8, 1));
             Console.WriteLine(findMaximum(3, 2, 3));
             Console.WriteLine(findMaximum(1, 1, 9));
             Console.WriteLine(findMaximum(1, 9, 9));
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("elapsed time: " + elapsedMs.ToString());
         }
     }
 }
